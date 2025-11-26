9bf636ad2aece56a85471a20f1350e4bcae51e74
dsog-stores <office.dsog@gmail.com>
2025-11-26 17:14:36 +0300

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,23 @@
+{
+  "name": "DSOG STORES",
+  "short_name": "DSOG",
+  "description": "Luxury Fashion & Accessories",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#000000",
+  "theme_color": "#FFD700",
+  "orientation": "portrait-primary",
+  "scope": "/",
+  "icons": [
+    {
+      "src": "https://i.postimg.cc/kMZ1jTww/Untitled-design-4-removebg-preview.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "https://i.postimg.cc/kMZ1jTww/Untitled-design-4-removebg-preview.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
