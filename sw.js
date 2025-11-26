79f7a9dea6920af0cb7723aec1096d011e575ead
dsog-stores <office.dsog@gmail.com>
2025-11-26 17:16:00 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'dsog-stores-v1.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // Add other critical pages if needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
